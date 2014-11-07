3971742fce0c19fa98c3fef67db5fe750f8c62a7
bryanh <bryanh@feynman.dice.ad>
2014-11-07 13:50:20 -0600

    added auth0

--- platforms/ios/www/js/main.js
+++ platforms/ios/www/js/main.js
@@ -19,8 +19,8 @@ var renderHome = function() {
         Handlebars.registerHelper('noop', function(options) {
           return timeSince(this.created_at * 1000);
         });
-        $.getJSON("http://www.roxlr.com/imgs", function(data) {
-                var template = "<ul class='row' style='width:340px;'>{{#.}}<li><span class='timeSince glyphicon glyphicon-time' style='float:right;'>{{#noop}}{{created_at}}{{/noop}}</span><div class='view'><img class='img-responsive' src='http://www.roxlr.com/uploads/{{account_id}}/{{photo_id}}.l.jpg'>" +
+        $.getJSON("http://www.roxlr.com:9293/imgs", function(data) {
+                var template = "<ul class='row' style='width:340px;'>{{#.}}<li><span class='timeSince glyphicon glyphicon-time' style='float:right;'>{{#noop}}{{created_at}}{{/noop}}</span><div class='view'><img class='img-responsive' src='http://www.roxlr.com:9293/uploads/{{account_id}}/{{photo_id}}.l.jpg'>" +
                                "</div><span class='glyphicon glyphicon-search'></span>{{/.}}</ul>";
                 var template = Handlebars.compile(template);
                 var html = template(data);
@@ -29,6 +29,35 @@ var renderHome = function() {
 }
 
 window.onload=function(){
+//document.addEventListener('deviceready',function() {
+var lock = null;
+lock = new Auth0Lock('J8CvsRkzaQyS6x89ZYQ0P0AUwyH2enSR', 'roxe.auth0.com');
+var userProfile;
+
+$('.btn-login').click(function(e) {
+  e.preventDefault();
+  lock.show(function(err, profile, token) {
+    if (err) {
+      // Error callback
+      alert('There was an error');
+    } else {
+      // Success calback
+      // Save the JWT token.
+      localStorage.setItem('userToken', token);
+      // Save the profile
+      userProfile = profile;
+    }
+  });
+   $.ajaxSetup({
+  'beforeSend': function(xhr) {
+    if (localStorage.getItem('userToken')) {
+      xhr.setRequestHeader('Authorization',
+            'Bearer ' + localStorage.getItem('userToken'));
+    }
+  }
+  });
+});
+
     $("#ajaxSpinnerContainer").hide();
     renderHome();
 }
@@ -58,3 +87,64 @@ function timeSince(date) {
     }
     return Math.floor(seconds) + "s";
 }
+
+document.addEventListener('deviceready',function() {
+    var widget = new Auth0Widget({
+        // All this properties are set on auth0-variables.js
+        domain: AUTH0_DOMAIN,
+        clientID: AUTH0_CLIENT_ID,
+        callbackURL: AUTH0_CALLBACK_URL,
+        callbackOnLocationHash: true
+    });
+
+    var userProfile;
+
+    $('.btn-login').click(function(e) {
+      e.preventDefault();
+      widget.signin({ popup: true} , null, function(err, profile, token) {
+        if (err) {
+          // Error callback
+          console.log("There was an error");
+          alert("There was an error logging in");
+        } else {
+          // Success calback
+
+          // Save the JWT token.
+          localStorage.setItem('userToken', token);
+
+          // Save the profile
+          userProfile = profile;
+
+          $('.login-box').hide();
+          $('.logged-in-box').show();
+          $('.nickname').text(profile.nickname);
+          $('.nickname').text(profile.name);
+          $('.avatar').attr('src', profile.picture);
+        }
+      });
+    });
+
+    $.ajaxSetup({
+      'beforeSend': function(xhr) {
+        if (localStorage.getItem('userToken')) {
+          xhr.setRequestHeader('Authorization',
+                'Bearer ' + localStorage.getItem('userToken'));
+        }
+      }
+    });
+
+    $('.btn-api').click(function(e) {
+      // Just call your API here. The header will be sent
+      $.ajax({
+        url: 'http://auth0-nodejsapi-sample.herokuapp.com/secured/ping',
+        method: 'GET'
+      }).then(function(data, textStatus, jqXHR) {
+        alert("The request to the secured enpoint was successfull");
+      }, function() {
+        alert("You need to download the server seed and start it to call this API");
+      });
+    });
+
+
+}, false);
+
---
+++ www/js/auth0-variables.js
@@ -0,0 +1,3 @@
+var AUTH0_CLIENT_ID='J8CvsRkzaQyS6x89ZYQ0P0AUwyH2enSR'; 
+var AUTH0_DOMAIN='roxe.auth0.com'; 
+var AUTH0_CALLBACK_URL=location.href;
